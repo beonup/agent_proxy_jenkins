d54057e7ce65b018741bcfe97f60cd61e3015a8e
ozeiasosilva <ozeias.osilva@gmail.com>
2022-02-15 17:11:08 -0300

    corrige startup

--- setup.sh
+++ setup.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 mkdir -p /var/jenkins
 
-wget https://zabbix-automation.beonup.com.br/jnlpJars/agent.jar -P /var/jenkins
+wget --no-check-certificate https://zabbix-automation.beonup.com.br/jnlpJars/agent.jar -P /var/jenkins
 
-echo "#!/bin/bash \
-cd /var/jenkins \
-/usr/bin/java -jar agent.jar -jnlpUrl $1 -secret $2 -workDir "/var/jenkins" >> /var/jenkins/agent.log 2>> /var/jenkins/agent.log" >> /var/jenkins/agent_beonup_startup.sh
+bash -c 'cat > /var/jenkins/agent_beonup_startup.sh' <<-EOF
+#!/bin/bash
+cd /var/jenkins
+/usr/bin/java -jar agent.jar -jnlpUrl $1 -secret $2 -workDir "/var/jenkins" >> /var/jenkins/agent.log 2>> /var/jenkins/agent.log
+EOF
 
 chmod +x /var/jenkins/agent_beonup_startup.sh
 
-echo "[Unit] \
-Description=Description for sample script goes here \
-After=network.target \
- \
-[Service] \
-Type=simple \
-ExecStart=/var/jenkins/agent_beonup_startup.sh \
-TimeoutStartSec=0 \
- \
-[Install] \
-WantedBy=default.target" > /etc/systemd/system/agent_beonup_startup.service
+bash -c 'cat > /etc/systemd/system/agent_beonup_startup.service' <<-EOF
+[Unit]
+Description=Description for sample script goes here
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/var/jenkins/agent_beonup_startup.sh
+TimeoutStartSec=0
+
+[Install]
+WantedBy=default.target
+EOF
 
 systemctl reload daemon
 systemctl start agent_beonup_startup
