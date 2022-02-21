37a897d5f4881477cbdb6e9bd28b2ba401654008
ozeiasosilva <ozeias.osilva@gmail.com>
2022-02-21 10:02:56 -0300

    melhoria plugin oracle

--- plugins/oracle/sql/oracle.jobs.status.sql
+++ plugins/oracle/sql/oracle.jobs.status.sql
@@ -1,4 +1,5 @@
-select STATUS from(
-select job_name,STATUS,max(log_Date) as log from DBA_SCHEDULER_JOB_RUN_DETAILS
-group by job_name,STATUS)
-where job_name like '%{#JOB_NAME}%'
+SELECT j.job_name AS JOBNAME, STATUS FROM DBA_SCHEDULER_JOB_RUN_DETAILS d, dba_scheduler_jobs j
+WHERE 
+d.job_name = j.job_name
+AND j.owner not in ('SYS','ORACLE_OCM','GTPLAN')
+GROUP BY j.job_name,d.STATUS
